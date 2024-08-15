f429863a9c4088895a7c483229c9805e57826bbc
Abrar Ahmad <kh.abrar22@gmail.com>
2024-08-15 19:26:21 +0500

    New: User registration and email verification

---
+++ src/app/api/sign-up/rout.ts
@@ -0,0 +1,93 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs"
+import { sendVerificationEmail } from "@/helpers/emailVarificationEmail";
+
+export async function POST(request: Request){
+    await dbConnect()
+
+    try {
+        const {username, email, password} = await request.json()
+        
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success: false,
+                message: "Username is already taken"
+            }, {status: 400})
+        }
+        
+
+        const existingUserbyEmail = await UserModel.findOne({
+            email,
+            isVerified: true
+        })
+
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        if (existingUserbyEmail) {
+            if (existingUserbyEmail.isVerified) {
+                return Response.json({
+                    success: false,
+                    message: "User already exist with this email"
+                }, {status: 400})
+            } else {
+                const hashedPassword = await bcrypt.hash(password, 10)
+                existingUserbyEmail.password = hashedPassword
+                existingUserbyEmail.verifyCode = verifyCode
+                existingUserbyEmail.verifyCodeExiry = new Date(Date.now() + 3600000)
+
+                await existingUserbyEmail.save()
+            }
+
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode: verifyCode,
+                verifyCodeExiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: []
+            })
+
+            await newUser.save()
+        }
+
+        //Send Varification email
+        const emailResponse = await sendVerificationEmail(email, username, verifyCode)
+
+        if (!emailResponse.success) {
+            return Response.json({
+                success: false,
+                message: emailResponse.message
+            }, {status: 500})
+        }
+
+        return Response.json({
+            success: true,
+            message: "Verify your email"
+        }, {status: 201})
+
+    } catch (error) {
+        console.error("Error registering user", error)
+        return Response.json(
+            {
+                success: false,
+                message: "Error registering user"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
---
+++ src/helpers/emailVarificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string,
+): Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Feedback App | Verification Code',
+            react: VerificationEmail({username, otp: verifyCode}),
+          });
+        
+        return {success: true, message: "Verification email sent successfully"}
+    } catch (emaiError) {
+        console.log("Error sending verification email", emaiError)
+        return {success: false, message: "Failed to send verification email"}
+    }
+}
