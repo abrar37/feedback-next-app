b21086fe22c84beb4871f899819b7c0a310293f0
Abrar Ahmad <kh.abrar22@gmail.com>
2024-08-19 20:23:27 +0500

    Added: SignUp functionality

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -9,5 +9,5 @@ export const usernameValidation = z
 export const signUpSchema = z.object({
     username: usernameValidation,
     email: z.string().email({message: "Invalid email address"}),
-    passowrd: z.string().min(6, {message: "Password must be at least 6 characters"})
+    password: z.string().min(6, {message: "Password must be at least 6 characters"})
 })
