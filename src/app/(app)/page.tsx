93d91f9d097182555e23b8724ab2bdd2801ec23d
Abrar Ahmad <kh.abrar22@gmail.com>
2024-08-25 22:59:11 +0500

    Fix: SignUp page issues

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -19,7 +19,7 @@ function Home() {
     <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
       <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
-            Dive into the World of Anonymous Feedback
+            Get Anonymous Feedback
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
             True Feedback - Where your identity remains a secret.
@@ -51,7 +51,7 @@ function Home() {
         </Carousel>
     </main>
     <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-      © 2023 True Feedback. All rights reserved.
+      © 2024 Anonymous Feedback. All rights reserved.
     </footer>
     </>
   )
